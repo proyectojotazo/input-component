41a5a94f9ec7950edd989f386effaaf8a0ec048d
Javier <javigh87@gmail.com>
2021-05-29 08:38:35 +0200

    Proyecto finalizado

--- README.md
+++ README.md
@@ -1,70 +1,72 @@
-# Getting Started with Create React App
+<!-- Please update value in the {}  -->
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+<h1 align="center">Input Styled Component</h1>
 
-## Available Scripts
+<div align="center">
+   Solution for a challenge from  <a href="http://devchallenges.io" target="_blank">Devchallenges.io</a>.
+</div>
 
-In the project directory, you can run:
+<div align="center">
+  <h3>
+    <a href="https://styled-input1987.netlify.app/">
+      Solution
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/challenges/TSqutYM4c5WtluM7QzGp">
+      Challenge
+    </a>
+  </h3>
+</div>
 
-### `yarn start`
+<!-- TABLE OF CONTENTS -->
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Table of Contents
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+- [Overview](#overview)
+  - [Built With](#built-with)
+- [How to use](#how-to-use)
+- [Contact](#contact)
+- [Acknowledgements](#acknowledgements)
 
-### `yarn test`
+<!-- OVERVIEW -->
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Overview
 
-### `yarn build`
+![screenshot](./screenshot1.png)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+### Built With
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+<!-- This section should list any major frameworks that you built your project using. Here are a few examples.-->
 
-### `yarn eject`
+- React
+- JSX
+- CSS3
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+## How To Use
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+To clone and run this application, you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+```bash
+# Clone this repository
+$ git clone https://github.com/proyectojotazo/input-component
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+# Install dependencies
+$ npm install
 
-## Learn More
+# Run the app
+$ npm start
+```
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+## Acknowledgements
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+<!-- This section should list any articles or add-ons/plugins that helps you to complete the project. This is optional but it will help you in the future. For exmpale -->
 
-### Code Splitting
+- [Steps to replicate a design with only HTML and CSS](https://devchallenges-blogs.web.app/how-to-replicate-design/)
+- [Node.js](https://nodejs.org/)
+- [Marked - a markdown parser](https://github.com/chjj/marked)
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
+## Contact
 
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- GitHub [proyectojotazo](https://github.com/proyectojotazo)
+- LinkedIn [Javier Guerrero](https://www.linkedin.com/in/javier-guerrero-huete-7032101b9/)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import Aside from "./components/Aside";
+import Input from "./components/Input";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+
+const phoneIco = <FontAwesomeIcon icon={faPhoneAlt} />
+const lockIco = <FontAwesomeIcon icon={faLock} />
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="web-content">
+      <Aside />
+      <div className="main-content">
+        <header className="header">
+          <h3 className="header__title">Inputs</h3>
+        </header>
+        <section className="buttons-content">
+          <div className="row">
+            <div className="card">
+              <Input />
+              <p className="card__info fourth">&lt;Input /&gt;</p>
+            </div>
+            <div className="card">
+              <input
+                placeholder="Placeholder"
+                className="md default-width fixed-hover"
+                type="text"
+                name="inp"
+              />
+              <label className="card__label" htmlFor="inp">
+                Label
+              </label>
+              <p className="card__info grey">&amp;:hover</p>
+            </div>
+            <div className="card">
+              <input
+                placeholder="Placeholder"
+                className="md default-width fixed-focus"
+                type="text"
+                name="inp"
+              />
+              <label className="card__label" htmlFor="inp">
+                Label
+              </label>
+              <p className="card__info grey">&amp;:focus</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="card">
+              <Input error />
+              <p className="card__info fourth">&lt;Input error/&gt;</p>
+            </div>
+            <div className="card">
+              <input
+                placeholder="Placeholder"
+                className="md default-width fixed-hover"
+                type="text"
+                name="inp"
+              />
+              <label className="card__label" htmlFor="inp">
+                Label
+              </label>
+              <p className="card__info grey">&amp;:hover</p>
+            </div>
+            <div className="card">
+              <input
+                placeholder="Placeholder"
+                className="md default-width fixed-focus-error"
+                type="text"
+                name="inp"
+              />
+              <label className="card__label error-text" htmlFor="inp">
+                Label
+              </label>
+              <p className="card__info grey">&amp;:focus</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="card">
+              <Input disabled />
+              <p className="card__info fourth">&lt;Input disabled/&gt;</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="card">
+              <Input helperText="Some interesting text" />
+              <p className="card__info fourth">
+                &lt;Input helperText=”Some interesting text” /&gt;
+              </p>
+            </div>
+            <div className="card">
+              <Input helperText="Some interesting text" error/>
+              <p className="card__info fourth">
+                &lt;Input helperText=”Some interesting text” error /&gt;
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="card">
+            <Input startIcon={phoneIco} />
+              <p className="card__info fourth">&lt;Input startIcon/&gt;</p>
+            </div>
+            <div className="card">
+            <Input endIcon={lockIco} />
+              <p className="card__info fourth">&lt;Input endIcon/&gt;</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="card">
+            <Input value="text" />
+              <p className="card__info fourth">&lt;Input value="text" /&gt;</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="card">
+            <Input size="sm"/>
+              <p className="card__info fourth">&lt;Input size="sm"/&gt;</p>
+            </div>
+            <div className="card">
+            <Input size="md"/>
+              <p className="card__info fourth">&lt;Input size="md"/&gt;</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="card full-width">
+            <Input fullWidth />
+              <p className="card__info fourth">&lt;Input fullWIdth/&gt;</p>
+            </div>
+          </div>
+          <div className="row auto-height">
+            <div className="card">
+            <Input multiline row="4" />
+              <p className="card__info fourth">&lt;Input multiline row="4"/&gt;</p>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
